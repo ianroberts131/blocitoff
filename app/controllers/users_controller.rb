ac6d6ed9b92f29294087db78b3a90de64d97cabf
ianroberts131 <ianroberts131@gmail.com>
2016-05-06 02:01:40 +0000

    Update users#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   def show
-    if params[:id]
-      @user = User.find(params[:id])
-    else
-      @user = current_user
-    end
+    @user = current_user
   end
 end
