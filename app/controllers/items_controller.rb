9cd444c09d2267f96c7aba940a6c704d3935701b
ianroberts131 <ianroberts131@gmail.com>
2016-05-09 00:45:00 +0000

    Add create to-do item functionality

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,20 @@
+class ItemsController < ApplicationController
+  
+  def create
+    @user = current_user
+    @item = @user.items.new(item_params)
+
+    if @item.save
+      flash[:notice] = "Item saved successfully."
+      redirect_to(authenticated_root_path)
+    else
+      flash[:alert] = "Items failed to save."
+    end
+  end
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   authenticated :user do
     root to: "users#show", as: :authenticated_root
+    resources :items, only: [:create]
+    resources :users, only: [:show]
   end
 
   root 'welcome#home'
-
 
 end
