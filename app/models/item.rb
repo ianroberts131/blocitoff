98f5562163a6e97cc578c8e84f8b01a916ef85e6
ianroberts131 <ianroberts131@gmail.com>
2016-05-13 03:17:26 +0000

    Refactor rake delete items task

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+  
+  LIFETIME_IN_SECONDS = 7.days
+  
+  def self.delete_expired
+    where("created_at <= ?", Time.now - LIFETIME_IN_SECONDS).destroy_all
+  end
 end
--- lib/tasks/todo.rake
+++ lib/tasks/todo.rake
@@ -1,7 +1,7 @@
 namespace :todo do
   desc "Deletes items older than 7 days"
   task delete_items: :environment do
-    Item.where("created_at <= ?", Time.now - 7.days).destroy_all
+    Item.delete_expired
   end
 
 end
